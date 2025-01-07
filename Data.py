628d2135a617a59925134621d585a1c7d6a9fbb3
yjoffeturjeman@cps.edu <yjoffeturjeman@cps.edu>
2025-01-07 10:17:34 -0600

    done

---
+++ Data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the Excel file into a DataFrame
+file_path = '\Users\<yjoffeturjeman@cps.edu>\Downloads\archive\Spotify Most Streamed Songs.csv'  # Replace with the actual file path
+df = pd.read_excel(file_path)
+
+
+
