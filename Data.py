b88751c8452e6ec2c2a359147cb38e6eea3f604d
yjoffeturjeman@cps.edu <yjoffeturjeman@cps.edu>
2025-01-09 10:22:41 -0600

    pollution

--- Data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the Excel file into a DataFrame
-file_path = '\Users\<yjoffeturjeman@cps.edu>\Downloads\archive\Spotify Most Streamed Songs.csv'  # Replace with the actual file path
-df = pd.read_excel(file_path)
-
-
-
