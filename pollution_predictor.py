85ed50aef2b331799ba1969483b20443d910bbf5
yjoffeturjeman@cps.edu <yjoffeturjeman@cps.edu>
2025-01-10 10:05:59 -0600

    equation

--- pollution_predictor.py
+++ pollution_predictor.py
@@ -3,8 +3,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("part3-multivariable-linear-regression/pollution.csv")
-x = data[["miles","age"]].values
+data = pd.read_csv("pollution.csv")
+x = data[["CO AQI Value","Ozone AQI Value","NO2 AQI Value","PM2.5 AQI Value"]].values
 y = data["AQI Value"].values
 
 #split the data into training and testing data
@@ -17,7 +17,7 @@ coef = np.around(model.coef_, 2)
 intercept =round((model.intercept_), 2)
 r_squared = round(model.score(x,y),2)
 
-print(f"The linear equation is y={coef[0]}x + {coef[1]}x2 + {intercept}")
+print(f"The linear equation is y={coef[0]}x + {coef[1]}x2 + {coef[2]}x3 + {coef[3]}x4 + {intercept}")
 print(f"R^2 value is {r_squared}")
 #Loop through the data and print out the predicted prices and the 
 predict = model.predict(xtest)
@@ -28,4 +28,4 @@ for index in range(len(xtest)):
     actual = ytest[index]
     predicted_y = predict[index]
     x_coord = xtest[index]
-    print(f"miles: {x_coord[0]} age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
+    print(f"CO AQI Value: {x_coord[2]} Ozone AQI Value: {x_coord[3]} NO2 AQI Value: {x_coord[3]} PM2.5 AQI Value: {x_coord[4]}")
